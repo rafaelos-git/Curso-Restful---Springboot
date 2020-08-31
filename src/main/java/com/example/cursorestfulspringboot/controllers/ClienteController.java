8712c6797d3d76494aad6432965304ec888e9649
RafaelOliveiraSilva <rafael.oliveiratdm@outlook.com>
2020-08-31 19:25:19 -0300

    #1 - Fix mensagem do resource /clientes

--- src/main/java/com/example/cursorestfulspringboot/controllers/ClienteController.java
+++ src/main/java/com/example/cursorestfulspringboot/controllers/ClienteController.java
@@ -7,12 +7,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class ClienteController {
 
-    @GetMapping("/cliente")
-    public String getCliente(){
-        return "Vai retornar todos os clientes";
+    @GetMapping("/clientes")
+    public String getClientes(){
+        return "Vai retornar todos os clientes da base";
     } 
 
-    @GetMapping("/cliente/{codigo}")
+    @GetMapping("/clientes/{codigo}")
     public String getClienteByCodigo(@PathVariable int codigo){
         return "Vai retornar o cliente de código: " + codigo;
     } 
