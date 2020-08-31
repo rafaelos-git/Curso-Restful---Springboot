8c112f83c7a83a3ccd7a9f74323c0d26fdb54858
RafaelOliveiraSilva <rafael.oliveiratdm@outlook.com>
2020-08-31 20:35:44 -0300

    #2 resource /cliente/{id}

---
+++ src/main/java/com/example/cursorestfulspringboot/controllers/Cliente.java
@@ -0,0 +1,8 @@
+package com.example.cursorestfulspringboot.controllers;
+
+public class Cliente {
+    public int id;
+    public String nome;
+    public String endereco;
+    public double saldo;
+}
--- src/main/java/com/example/cursorestfulspringboot/controllers/ClienteController.java
+++ src/main/java/com/example/cursorestfulspringboot/controllers/ClienteController.java
@@ -1,20 +1,57 @@
 package com.example.cursorestfulspringboot.controllers;
 
+import java.util.Arrays;
+import java.util.List;
+
+import javax.annotation.PostConstruct;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class ClienteController {
+    public List<Cliente> clientes;
+
+    @PostConstruct
+    public void init(){
+        Cliente c1 = new Cliente();
+        c1.id = 1;
+        c1.nome = "Rafael";
+        c1.endereco = "Rua Guarda Civil, 182 - Vila Barão";
+        c1.saldo = 1000000.00;
+
+        Cliente c2 = new Cliente();
+        c2.id = 2;
+        c2.nome = "Edson";
+        c2.endereco = "Rua Guarda Civil, 182 - Vila Barão";
+        c2.saldo = 500000.00;
+
+        Cliente c3 = new Cliente();
+        c3.id = 3;
+        c3.nome = "Márcia";
+        c3.endereco = "Rua Guarda Civil, 182 - Vila Barão";
+        c3.saldo = 900000.00;
+
+        clientes = Arrays.asList(c1,c2,c3);
+    }
 
     @GetMapping("/clientes")
-    public String getClientes(){
-        return "Vai retornar todos os clientes da base";
+    public List<Cliente> getClientes(){
+        return clientes;
     } 
 
-    @GetMapping("/clientes/{codigo}")
-    public String getClienteByCodigo(@PathVariable int codigo){
-        return "Vai retornar o cliente de código: " + codigo;
+    @GetMapping("/clientes/{id}")
+    public Cliente getClienteByCodigo(@PathVariable int id){
+        Cliente cliente = null;
+
+        for(Cliente aux : clientes){
+            if(aux.id == id){
+                cliente = aux;
+                break;
+            }
+        }
+        return cliente;
     } 
 
 }
